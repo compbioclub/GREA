ca1889d9b2bf5f6553656bac7ab802451c35750f
fanjiangchenghe <931864095@qq.com>
2025-03-13 12:01:25 +0800

    add benchmark

--- grea/__init__.py
+++ grea/__init__.py
@@ -2,5 +2,14 @@ from .grea import (
     GREA
 )
 from .plot import (
-    running_sum
+    running_sum,
+    top_table,
+    plot_gene_heatmap,
+    plot_box
+)
+from .benchmarking import (
+    benchmark_parallel
+)
+from .library import (
+    read_gmt
 )
