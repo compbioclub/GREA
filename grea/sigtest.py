32c7b28ac024e269fd82224876c0abdcbd2fe24a
fanjiangchenghe <931864095@qq.com>
2025-03-14 15:52:59 +0800

    delete null_rs

--- grea/sigtest.py
+++ grea/sigtest.py
@@ -14,7 +14,7 @@ from statsmodels.stats.multitest import multipletests
 import seaborn as sns
 
 
-import enrich as enrich
+import grea.enrich as enrich
 
 from mpmath import mp, exp, log
 import math
