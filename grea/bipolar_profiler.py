fc143d237d45b5642d37738b2f935860c1bf49fe
thelingxichen <chanlingxi@gmail.com>
2025-06-03 17:17:48 +0800

    fix bipolar_profile bug: union to intersect when pred_prob

--- grea/bipolar_profiler.py
+++ grea/bipolar_profiler.py
@@ -7,7 +7,9 @@ from matplotlib import pyplot as plt
 from grea import grea
 
 
-def profile_bipolarity(rank_df, libraries, n_perm=1000):
+def profile_bipolarity(rank_df, libraries, 
+                       high='high', low='low',
+                       n_perm=1000, prefix=''):
 
 
     obj = grea.obs_prerank_enrich(rank_df, libraries, center=False, add_noise=False, n_perm=n_perm)
@@ -17,20 +19,22 @@ def profile_bipolarity(rank_df, libraries, n_perm=1000):
         if col.endswith('_-'):
             df[col] = 1 - df[col]
 
-    return df
+    t_results, perf_results, pred_results = prob_test(
+        df, n_perm=n_perm, high=high, low=low, prefix=prefix)
+
+    return df, t_results, pred_results
 
 
-def prob_test(df, probs=np.arange(100, 0, -10), n_perm=1000):
+def prob_test(df, probs=np.arange(100, 0, -10), n_perm=1000,
+              high='high', low='low', prefix=''):
                 
     perf_results = pd.DataFrame()
     pred_results = pd.DataFrame(index=df.index)
     
     for prob in probs:
-        print(f'prob {prob}')
         t_perf, common_preds = _random_permutation_test_by_prob(df, prob=prob, n_perm=n_perm)
-        perf_results[f'prob_{prob}%'] = t_perf
-            
-        pred_results[f'prob_{prob}%'] = ['high' if x in common_preds else 'low' for x in df.index]
+        perf_results[f'prob_{prob}%'] = t_perf            
+        pred_results[f'prob_{prob}%'] = [high if x in common_preds else low for x in df.index]
     
     p_value = perf_results.apply(lambda x: np.sum(x[1:] >= x[0]) / len(x[1:]), axis=0)
     medianN = perf_results.iloc[1:, :].median(axis=0)
@@ -54,16 +58,17 @@ def prob_test(df, probs=np.arange(100, 0, -10), n_perm=1000):
 
     t_results.plot(y='Rcm', kind='bar', ylim=(0, 2), ylabel='Rcm value', color='peachpuff')
     plt.axhline(y=1, linestyle='dotted', linewidth=3, color='red')
+    plt.title(prefix)
     plt.show()    
     return t_results, perf_results, pred_results
             
-def _random_permutation_test_by_prob(df, prob=50, n_perm=10, random_seed=0):
-    np.random.seed(random_seed)
+def _random_permutation_test_by_prob(df, prob=50, n_perm=10):
     t_perf = []
     N = df.shape[0]
     for i in range(n_perm):
         shuffled_df = df.copy()  
-        for col in shuffled_df.columns:
+        for i, col in enumerate(shuffled_df.columns):
+            np.random.seed(i)
             shuffled_df[col] = np.random.permutation(shuffled_df[col])  # Shuffle the column
 
         random_selected = _predict_by_prob(shuffled_df, prob)              
@@ -76,8 +81,5 @@ def _random_permutation_test_by_prob(df, prob=50, n_perm=10, random_seed=0):
     return t_perf, common_preds            
     
 def _predict_by_prob(df, prob):
-    preds = []
-    for col in df.columns:
-        f = df[col]
-        preds += df[f > prob*0.01].index.tolist()
-    return set(preds)
+    preds = df[(df>prob*0.01).all(axis=1)].index.tolist()
+    return preds
